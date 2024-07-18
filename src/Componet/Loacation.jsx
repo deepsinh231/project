d14f4d248731774c9e5f3ef5ad41060044bbea33
deepsinh231 <deepsinh231@gmail.com>
2024-07-18 20:48:36 +0530

    map

--- src/Componet/Loacation.jsx
+++ src/Componet/Loacation.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from 'react';
-import GoogleMapReact from 'google-map-react';
+// import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 const Current_weather_data = () => {
     const [citymain, setcity] = useState(null);
     const [lanlat, setcitylanlat] = useState(null);
     const [fail, setfaillocatin] = useState(null);
     const [datamap, setdatamap] = useState(null);
+    const [datagetset, setdatagetset] = useState(null);
     const [search, setsearch] = useState('');
-    const defaultProps = {
-        center: {
-            lat: 10.99835602,
-            lng: 77.01502627
-        },
-        zoom: 11
-    };
+    // const defaultProps = {
+    //     center: {
+    //         lat: 10.99835602,
+    //         lng: 77.01502627
+    //     },
+    //     zoom: 11
+    // };
     useEffect(() => {
         const findcity = async () => {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=e322d30c923c4f26c82811389689a883`;
@@ -27,12 +28,16 @@ const Current_weather_data = () => {
         findcity();
     }, [search])
     const CurrentLocationdat = async (lon, lat) => {
+        setdatagetset("wait")
+        setdatamap(`https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7342.633047467606!2d${lon}!3d${lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1689624379247!5m2!1sen!2sin`)
+
         const urls = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=6db7852bc3356870fcc7e4a0acec2839`
         const respo = await fetch(urls);
         const resjosn = await respo.json();
         setcity(resjosn.main)
         setcitylanlat(resjosn.coord)
-        setdatamap(`https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7342.633047467606!2d${lon}!3d${lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1689624379247!5m2!1sen!2sin`)
+        setdatagetset("")
+
 
     }
     const getCurrentLocation = async (Position) => {
@@ -45,13 +50,22 @@ const Current_weather_data = () => {
     const CurrentLocation = async () => {
         navigator.geolocation.getCurrentPosition(getCurrentLocation, faillocatin)
     }
+    console.log(datamap);
     return (
         <>
             <div className="p-10">
                 <input type="search" placeholder='Any city name' className='border' value={search} onChange={(e) => { setsearch(e.target.value) }} />
-                <button onClick={CurrentLocation}>Current Location</button><span className='failclass'>{fail}</span>
+                <button onClick={CurrentLocation}>Current Location</button>
+                <br />
+                <span className='failclass'>{datagetset}</span>
+                <br />
+                <span className='failclass'>{fail}</span>
                 {!citymain ? <p>no data found</p> : <div>
-                    <p>City Name is <span className='weather'>{search}</span></p>
+                    {search.length !== 0 ?
+                        <p>City Name is <span className='weather'>{search}</span></p>
+                        :
+                        ""
+                    }
                     <p>temp_max is <span className='weather'>{citymain["temp_max"]}</span>Cel</p>
                     <p>temp_min is <span className='weather'>{citymain["temp_min"]}</span>Cel</p>
                     <p>Lon  <span className='weather'>{lanlat['lon']}</span></p>
@@ -59,22 +73,28 @@ const Current_weather_data = () => {
 
                 </div>}
                 {
-                    datamap &&
-                    <div style={{ height: '100vh', width: '100%' }}>
-                        <GoogleMapReact
-                        // https://maps.googleapis.com/maps/api/js?key=&libraries=places&callback=initMap
-                            bootstrapURLKeys={{ key: "AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao" }}
-                            defaultCenter={defaultProps.center}
-                            defaultZoom={defaultProps.zoom}
-                        >
-                            <AnyReactComponent
-                                lat={59.955413}
-                                lng={30.337844}
-                                text="My Marker"
-                            />
-                        </GoogleMapReact>
-                    </div>}
-            </div>
+                    citymain &&
+                    <div className="">
+                        <iframe src={datamap}
+                            width="600" height="450" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe className='w-full max-h-96' ></iframe>
+                    </div>
+                    // <div style={{ height: '100vh', width: '100%' }}>
+                    //     <GoogleMapReact
+                    //         // https://maps.googleapis.com/maps/api/js?key=&libraries=places&callback=initMap
+                    //         bootstrapURLKeys={{ key: "AIzaSyBxMFGggAwRDGvDEegqhvystV88sQeZ-xY" }}
+                    //         defaultCenter={defaultProps.center}
+                    //         defaultZoom={defaultProps.zoom}
+                    //     >
+                    //         <AnyReactComponent
+                    //             lat={59.955413}
+                    //             lng={30.337844}
+                    //             text="My Marker"
+                    //         />
+                    //     </GoogleMapReact>
+                    // </div>
+                }
+            </div >
         </>
     );
 };
